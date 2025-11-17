0869d9e66b1b75cf1b670ee8efd15749b910b7fd
AdrianoSoll <solleradriano@gmail.com>
2025-11-16 23:50:15 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANTE: Esta línea le dice a Vite que use el nombre del repositorio como ruta base.
-  base: '/frontend-factoring/',
-})

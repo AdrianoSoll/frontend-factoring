7cc15cbd6b8a32b60a12c2a678bbe05a12e3ce6c
Adriano <solleradriano@gmail.com>
2025-11-18 18:03:49 -0500

    Descripción concisa de los cambios realizados

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/frontend-factoring">
       <App />
     </BrowserRouter>
   </React.StrictMode>
